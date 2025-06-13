d9d9de0f132e08291f2610d015db8c51e1c57767
alesandrshy <alesandrshyti454@gmail.com>
2025-06-13 07:41:37 +0000

    deploy V0.0.1

---
+++ src/settings.52b15.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default","game3d"],collisionMatrix:[[true,true],[true,true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/resources/Scene/loading.fire",orientation:"portrait",jsList:[],bundleVers:{remote:"825a7",resources:"1f45d",res:"0718e",internal:"d0832",main:"da0db"}};
